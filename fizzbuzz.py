61b61fa40d7e66008ab55f13ec82d57a2ef0a178
Joydeep Chatterjee <chatterjee.prime@gmail.com>
2016-10-05 16:31:59 +0000

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+n = 100
+x = 1
+
+print("Fizz Buzz counting up to {}".format(n))
+
+while x <= n:
+    if x % 15 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
+    x += 1
