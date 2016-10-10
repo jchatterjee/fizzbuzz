cf500ccfd347ec4d132233f95043179f4c3fec61
Joydeep Chatterjee <chatterjee.prime@gmail.com>
2016-10-10 15:05:42 +0000

    v.5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,23 @@
 import sys
 
-try:
-    if sys.argv[1:]:
-        for arg in sys.argv[1:]:
-            n = int(arg)
-    else:
-        n = int(input("How high do you want to count? "))
-except ValueError:
-    print("Please enter a positive integer.")
-    n = int(input("How high do you want to count? "))
+if sys.argv[1:]:
+    for arg in sys.argv[1:]:
+        user_n = arg
+else:
+    user_n = input("How high do you want to count? ")
 
+while True: 
+    try:
+        n = int(user_n)
+        if n > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+            user_n = input("How high do you want to count? ")
+    except ValueError:
+        print("Please enter a positive integer.")
+        user_n = input("How high do you want to count? ")
+        
 x = 1
 
 print("Fizz Buzz counting up to {}".format(n))
